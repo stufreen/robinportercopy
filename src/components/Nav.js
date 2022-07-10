0d35cc2d04ef7ee777b948dc2e3fad2152936982
Stu Freen <stufreen@gmail.com>
2022-07-10 13:09:32 -0400

    Improve menu

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,40 @@
-const nav = document.getElementById("nav");
-const toggle = document.getElementById("nav-toggle");
-const drawer = document.getElementById("nav-drawer");
+const opener = document.getElementById("menu-opener");
+const closer = document.getElementById("menu-closer");
+const drawer = document.getElementById("menu-drawer");
+const animatedItems = document.querySelectorAll("[data-menu-animated]");
 
-toggle.addEventListener("click", () => {
-  if (drawer.classList.contains("open")) {
-    drawer.classList.remove("open");
-    toggle.classList.remove("open");
-    toggle.setAttribute("aria-expanded", "false");
-  } else {
-    drawer.classList.add("open");
-    toggle.classList.add("open");
-    toggle.setAttribute("aria-expanded", "true");
+console.log(animatedItems);
+
+opener.addEventListener("click", () => {
+  drawer.classList.add("open");
+  document.body.classList.add("mobile-drawer-open");
+
+  for (let i = 0; i < animatedItems.length; i++) {
+    animatedItems[i].style.opacity = 0;
+    animatedItems[i].animate(
+      [
+        { transform: "translateY(-0.3rem) rotateX(50deg)", opacity: 0 },
+        { transform: "translateY(0rem) rotateX(0deg)", opacity: 1 },
+      ],
+      {
+        duration: 300,
+        delay: 30 * i,
+        easing: "cubic-bezier(0.33, 1, 0.68, 1)",
+        fill: "both",
+      }
+    );
+    // animatedItems[i].style.opacity = 0;
+    // animatedItems[i],
+    //   300,
+    //   { transform: { translateX: "-0.5rem", rotateX: "10deg" }, opacity: 0 },
+    //   { transform: { translateX: "0rem", rotateX: "0deg" }, opacity: 1 },
+    //   animol.Easing.easeOutCubic,
+    //   30 * i
+    // );
   }
+});
+
+closer.addEventListener("click", () => {
+  drawer.classList.remove("open");
+  document.body.classList.remove("mobile-drawer-open");
 });
