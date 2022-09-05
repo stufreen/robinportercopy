e5181c4908221bcfdc86c3e3ede705578d263bbf
Stu Freen <stufreen@gmail.com>
2022-09-05 09:14:47 -0400

    Fix utils

--- src/utils/getProjects.js
+++ src/utils/getProjects.js
@@ -1,48 +1,5 @@
 import { client } from "./contentful";
 
-// export const getProjects = async () => {
-//   const response = await fetch(CONTENTFUL_URL, {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//       Authorization: `Bearer ${CONTENTFUL_ACCESS_TOKEN}`,
-//     },
-//     body: JSON.stringify({
-//       query: `
-//         {
-//           projectCollection {
-//             total
-//             items {
-//               title
-//               slug
-//               shortDescription {
-//                 json
-//               }
-//               longDescription {
-//                 json
-//               }
-//               coverImage {
-//                 title
-//                 url
-//               }
-//             }
-//           }
-//         }
-//       `,
-//     }),
-//   });
-//   const json = await response.json();
-
-//   if (!json.data?.projectCollection) {
-//     console.error("No projectCollection", JSON.stringify(json));
-//     return {
-//       items: [],
-//     };
-//   }
-
-//   return json.data.projectCollection;
-// };
-
 export const getProjects = () =>
   client
     .getEntries({
